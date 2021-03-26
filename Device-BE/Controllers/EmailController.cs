6a7ed4e4af6d361eac2666a3b4400db4bb563486
CuongNB <CuongNB@DESKTOP-90R2L20>
2021-03-26 17:32:08 +0700

    add email

---
+++ Device-BE/Controllers/EmailController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Device_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private static readonly string _from = ""; // Email của Sender (của bạn)
+        private static readonly string _pass = ""; // Mật khẩu Email của Sender (của bạn)
+
+        public static string Send(string sendto, string subject, string content)
+        {
+            //sendto: Email receiver (người nhận)
+            //subject: Tiêu đề email
+            //content: Nội dung của email, bạn có thể viết mã HTML
+            //Nếu gửi email thành công, sẽ trả về kết quả: OK, không thành công sẽ trả về thông tin l�-i
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(_from);
+                mail.To.Add(sendto);
+                mail.Subject = subject;
+                mail.IsBodyHtml = true;
+                mail.Body = content;
+
+                mail.Priority = MailPriority.High;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(_from, _pass);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+        }
+    }
+}
